75ef19924f62d87b117817f5fd6875f9d47bb931
Ricardo <ricardo.abarza.v@zoho.com>
2021-01-09 10:02:23 -0300

    creacion del proyecto en eclipse

---
+++ src/main/java/com/web/registroCapaciacion/RegistroCapacitacionApplication.java
@@ -0,0 +1,13 @@
+package com.web.registroCapaciacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroCapacitacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroCapacitacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/registroCapaciacion/RegistroCapacitacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.registroCapaciacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroCapacitacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

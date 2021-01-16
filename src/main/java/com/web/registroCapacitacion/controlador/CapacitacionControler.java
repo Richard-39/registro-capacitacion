147821ae2e86a3e3c4c7365eb89bfd6d6eda7d15
richard-39 <ricardo.abarza.v@zoho.com>
2021-01-15 21:31:49 -0300

    vista de capacitaciones y agregar asistentes

--- src/main/java/com/web/registroCapacitacion/controlador/CapacitacionControler.java
+++ src/main/java/com/web/registroCapacitacion/controlador/CapacitacionControler.java
@@ -7,9 +7,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.web.registroCapacitacion.modelo.Capacitacion;
+import com.web.registroCapacitacion.servicio.IAsistente;
 import com.web.registroCapacitacion.servicio.ICapacitacion;
 import com.web.registroCapacitacion.servicio.IExpositor;
 import com.web.registroCapacitacion.servicio.IGrupo;
@@ -18,6 +22,7 @@ import com.web.registroCapacitacion.servicio.ITematica;
 
 @Controller
 @RequestMapping("/capacitaciones")
+@SessionAttributes("capacitacionACompletar")
 public class CapacitacionControler {
 
 	@Autowired
@@ -34,6 +39,9 @@ public class CapacitacionControler {
 	
 	@Autowired
 	ILugar lugarServicio;
+	
+	@Autowired
+	IAsistente asistenteServicio;
 		
 	@GetMapping({"/", ""})
 	public String capacitaciones(Model model) {
@@ -62,11 +70,35 @@ public class CapacitacionControler {
 		
 		capacitacionServicio.save(capacitacion);
 		
-		System.out.println(capacitacion.getTematicas().get(0).getCapacitaciones());
-		
-		
 		
 		return new ModelAndView("redirect:/capacitaciones");
 	}
 	
+	@GetMapping("/ver")
+	public String ver(Model model, @RequestParam Integer id) {
+		
+		model.addAttribute("capacitacionVo", capacitacionServicio.findById(id));
+		
+		return "verCapacitacion";
+	}
+	
+	
+	@GetMapping("/preInscribir")
+	public String preInscribir(Model model, @RequestParam Integer id) {
+		
+		model.addAttribute("asistenteVo", asistenteServicio.findAll());
+		model.addAttribute("capacitacionACompletar", capacitacionServicio.findById(id).getCapacitaciones().get(0));
+		return "inscribirAsistente";
+	}
+	
+	@GetMapping("/inscribir")
+	public ModelAndView inscribir(@ModelAttribute("capacitacionACompletar") Capacitacion capacitacion, RedirectAttributes redirectAttr, SessionStatus status) {
+		
+		capacitacionServicio.update(capacitacion);
+		
+		// lo añade como get por la url
+		redirectAttr.addAttribute("id", capacitacion.getId_capacitacion());
+		status.setComplete();
+		return new ModelAndView("redirect:/capacitaciones/preInscribir");
+	}
 }
